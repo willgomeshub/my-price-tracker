100d736b80ccc1e72441f5aec360dbf3d7ba4785
willgomeshub <wabgomes2@gmail.com>
2025-02-19 14:15:57 -0300

    spring boot initializr

---
+++ backend/src/main/java/com/willgomeshub/my_price_tracker/MyPriceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.willgomeshub.my_price_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPriceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPriceTrackerApplication.class, args);
+	}
+
+}
